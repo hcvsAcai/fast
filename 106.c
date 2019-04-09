c76ed5c6961dc2c2a13a0a2108efb8a02dea1056
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-09 20:47:15 +0800

    Create 106.c

---
+++ 106.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () 
+{
+     int a, b, c, d;
+     printf("請輸入第一個整數? ");
+     scanf("%d", &a);
+     printf("請輸入第二個整數? ");
+     scanf("%d", &b); 
+     printf("請輸入第三個整數? ");
+     scanf("%d", &c);
+     printf("請輸入第四個整數? ");
+     scanf("%d", &d);
+
+     printf("此式的餘數為%d\n", ((a+b)/2-(c+d)/2 )% 2);//四哲運算規則
+     return 0;
+}
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int main () 
+{
+     int a, b, c, d;
+     printf("請輸入第一個整數? ");
+     scanf("%d", &a);
+     printf("請輸入第二個整數? ");
+     scanf("%d", &b); 
+     printf("請輸入第三個整數? ");
+     scanf("%d", &c);
+     printf("請輸入第四個整數? ");
+     scanf("%d", &d);
+
+     printf("此式的餘數為%d\n", a+b/2-c+d/2 % 2);
+     return 0;
+}*/

3a2ee8623d767ef9c299517bbdc0e60987dc2a75
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-29 20:36:52 +0800

    Create 710.c

---
+++ 710.c
@@ -0,0 +1,55 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+/* 若沒有定義Knum, 則加以定義為1000 */
+#ifndef Knum
+#define Knum 1000
+#endif
+/* 將Knum解除定義, 之後再定義為200 */
+#undef Knum
+#define Knum 200
+
+int main () 
+{
+/* 印出最後的Knum值 */
+     printf("Knum = %d\n", Knum);
+/* 下列的變數d和i共用8個Bytes, 不是12個Bytes */
+     union dataType {
+          double d;
+          int i;
+     };
+     struct dataType dT;
+     printf("請輸入d和i的變數值: ");
+     scanf("%lf %d", &dT.d, &dT.i); 
+/* 印出dT變數中的d和i */
+     printf("%f %d ", dT.d, dT.i);
+     return 0;
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+/* 若沒有定義Knum, 則加以定義為1000 */
+#ifndef Knum
+#define Knum 1000
+#endif
+/* 將Knum解除定義, 之後再定義為200 */
+#undef Knum
+#define Knum 200
+
+int main () 
+{
+/* 印出最後的Knum值 */
+     printf("Knum = %d\n", Knum);
+/* 下列的變數d和i共用8個Bytes, 不是12個Bytes */
+     union dataType {
+          double d;
+          int i;
+     };
+     union dataType dT;
+     printf("請輸入d的變數值: ");//將兩變數分開輸入和顯示
+ 	scanf("%lf", &dT.d);  
+ 	printf("%lf\n", dT.d);
+ 	printf("請輸入i的變數值: ");
+ 	scanf("%d", &dT.i);  
+/* 印出dT變數中的d和i */
+     printf("%d ", dT.i);
+     return 0;
+}

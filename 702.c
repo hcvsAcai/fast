c9d66579a472f3f046e707d28ba2fdad3501aa22
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-29 20:33:35 +0800

    Create 702.c

---
+++ 702.c
@@ -0,0 +1,24 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+int main () 
+{
+    char string[20];
+    double output;
+    printf("請輸入一數字的字串: ");
+    scanf("%s", string);
+    output=atoi(string);
+    printf("%s轉換後的整數為%f\n", string, output);
+    return 0;
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+int main () 
+{
+    char string[20];
+    int output;
+    printf("請輸入一數字的字串: ");
+    scanf("%s", string);
+    output=atoi(string);
+    printf("%s轉換後的整數為%d\n", string, output);
+    return 0;
+}

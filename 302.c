6977892ec6e812546432b31b53f45037c3d30ac2
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-29 20:40:17 +0800

    Create 302.c

---
+++ 302.c
@@ -0,0 +1,45 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+int adjust(int);
+int main () 
+{
+     int score, final;
+     printf("請輸入您的分數: ");
+     scanf("%d", &score);
+
+     adjust(score);
+
+     printf("調整後的分數: %d", final);
+     return 0;
+}
+
+int adjust(int score)
+{
+     if (score >= 60) 
+          temp = score + 10;
+     else 
+          temp = score + 5;
+
+     return temp;
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+int adjust(int);
+int main ()
+{
+ int score, final;
+ printf("請輸入您的分數: ");
+ scanf("%d", &score);
+ final = adjust(score);
+ printf("調整後的分數: %d", final);
+ return 0;
+}
+int adjust(int score)
+{
+ int temp;
+ if (score >= 60)
+  temp = score + 5;
+ else
+  temp = score + 10;
+ return temp;
+}

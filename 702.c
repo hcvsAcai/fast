104154e4fd404ca66fc45fde42610df9c9a57870
huang0216 <48470704+huang0216@users.noreply.github.com>
2019-04-29 21:24:42 +0800

    Update 702.c

--- 702.c
+++ 702.c
@@ -3,11 +3,11 @@
 int main () 
 {
     char string[20];
-    double output;
+    double output;//int
     printf("請輸入一數字的字串: ");
     scanf("%s", string);
     output=atoi(string);
-    printf("%s轉換後的整數為%f\n", string, output);
+    printf("%s轉換後的整數為%f\n", string, output);//%d
     return 0;
 }*/
 #include <stdio.h>

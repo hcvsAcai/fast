cdb985075a8e238360c5790ce9704fa77e4482db
huang0216 <48470704+huang0216@users.noreply.github.com>
2019-04-29 21:39:39 +0800

    Update 708.c

--- 708.c
+++ 708.c
@@ -1,15 +1,15 @@
 /*#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define random(x) rand()%x
+#define random(x) rand()%x //x+1
 int main() 
 {
      int i, j;
      int x;
      printf("隨機數的最大值為: ");
      scanf("%d", &x);
-     for (i=1; i<5; i++) {
-          for (j=1; j<4; j++) {
+     for (i=1; i<5; i++) { //i=0
+          for (j=1; j<4; j++) { //j=0
                printf("%5d ", random(x));
           }
           printf("\n");

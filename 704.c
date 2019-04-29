5619e35eb26f87247450e420a6caa9e7b863f37c
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-29 23:04:50 +0800

    Update 704.c

--- 704.c
+++ 704.c
@@ -21,7 +21,7 @@ int main ()
      printf("請輸入一個整數: ");
      scanf("%d", &num);
      triple_num = TRIPLE(num);
-     printf("%d的三次方為%d\n", num,triple_num);
+     printf("%d的三次方為%d\n", num,triple_num);//num
      triple_num = TRIPLE((4+1));
      printf("5的三次方為%d\n", triple_num);
      return 0;

7e0709c13f9a7072c7d99a14689640c6dc9e496e
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-29 21:31:38 +0800

    Update 308.c

--- 308.c
+++ 308.c
@@ -1,15 +1,29 @@
 /*#include <stdio.h>
 #include <stdlib.h>
 
-int main()
+double sum(double a[], int n);
+int main () 
 {
-     int arr[5]={10, 20, 30, 40, 50, 60};
-     int i, total=0;
-     for (i=1; i<6; i++) {
-          total += arr[i];
+     double arr[5];
+     int i, total;
+     for(i=0; i<5; i++) {
+          printf("請輸入陣列arr[%d]元素值: ", i);
+          scanf("%lf", arr[i]);
      }
-     printf("總和為%f\n", total);
+     for(i=0; i<5; i++)
+          printf("arr[%d]=%.2f\n", i, arr[i]);
+     total=sum(arr);
+     printf("此陣列的總和為%.2f\n", total);
      return 0;
+}
+
+double sum(int a[], int n)
+{
+     int k;
+     double tot;
+     for(k=1; k<n; k++)
+          tot += a[k];
+     return tot;
 }*/
 #include <stdio.h>
 #include <stdlib.h>

54818aa2a6c12c381dcfea4a60eeccc2b6beb0e2
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-29 20:41:21 +0800

    Create 304.c

---
+++ 304.c
@@ -0,0 +1,41 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+void callFun();
+int main()
+{
+     int i;
+     for (i=1; i<=5; i++) {
+          callFun();
+     }
+     return 0;
+}
+
+void callFun()
+{
+     int ai=100, si=100;
+     ai++;
+     si++;
+     printf("ai=%d, si=%d", ai, si);
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void callFun();
+int main()
+{
+     int i;
+     for (i=1; i<=5; i++) {
+          callFun();
+     }
+     return 0;
+}
+
+void callFun()
+{
+     static int  si=100;
+     auto int   ai=100;
+     ai++;
+     si++;
+     printf("ai=%d, si=%d\n", ai, si);
+}
